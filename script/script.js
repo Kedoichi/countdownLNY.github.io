edd02971d0b990c253a14e9da7c59d8eacbee87b
Kedoichi <01664841664q@gmail.com>
2021-02-20 07:08:59 +0700

    Update New Year List  for automaticly detect date for the next year

--- script/script.js
+++ script/script.js
@@ -3,19 +3,51 @@ const hourCD = document.getElementById("hours");
 const minCD = document.getElementById("mins");
 const secCD = document.getElementById("secs");
 
-const newYear = "01 Feb 2022";
-
-function countdown() {
-  const newYearDate = new Date(newYear);
+let newYearList = [];
+function newYearObj(year, newYearDate) {
+  this.year = year;
+  this.newYearDate = newYearDate;
+}
+newYearList.push(new newYearObj("2018", "16 Feb 2018"));
+function checkYear(year) {
+  return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+}
+function isValidTime() {
+  const newYearDate = new Date(newYearList[newYearList.length - 1].newYearDate);
   const currentDate = new Date();
-
+  const totalSec = (newYearDate - currentDate) / 1000;
+  if (totalSec < 0) {
+    if (checkYear(Number(newYearList[newYearList.length - 1].year)))
+      newYearList.push(
+        new newYearObj(
+          Number(newYearList[newYearList.length - 1].year) + 1,
+          new Date(newYearList[newYearList.length - 1].newYearDate).addDays(384)
+        )
+      );
+    else
+      newYearList.push(
+        new newYearObj(
+          Number(newYearList[newYearList.length - 1].year) + 1,
+          new Date(newYearList[newYearList.length - 1].newYearDate).addDays(354)
+        )
+      );
+    isValidTime();
+  }
+  return true;
+}
+Date.prototype.addDays = function (days) {
+  var date = new Date(this.valueOf());
+  date.setDate(date.getDate() + days);
+  return date;
+};
+function countdown() {
+  const newYearDate = new Date(newYearList[newYearList.length - 1].newYearDate);
+  const currentDate = new Date();
   const totalSec = (newYearDate - currentDate) / 1000;
   const day = Math.floor(totalSec / 3600 / 24);
   const hour = Math.floor(totalSec / 3600) % 24;
   const min = Math.floor(totalSec / 60) % 60;
   const sec = Math.floor(totalSec) % 60;
-
-  console.log(day, hour, min, sec);
 
   dayCD.innerHTML = formatTime(day);
   hourCD.innerHTML = formatTime(hour);
@@ -26,5 +58,8 @@ function countdown() {
 function formatTime(time) {
   return time < 10 ? `0${time}` : time;
 }
-countdown();
-setInterval(countdown, 1000);
+if (isValidTime()) {
+  console.log(newYearList);
+  countdown();
+  setInterval(countdown, 1000);
+}
